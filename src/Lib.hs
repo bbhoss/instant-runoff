4b6392bfa34fafaeacd21f7936b21b8644475d95
Preston Marshall <git@bbhoss.io>
2021-03-22 14:24:17 -0400

    return multiple rounds, add red spec

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,25 +7,34 @@ module Lib
 
 import Data.List
 import Data.Ord
+import Debug.Trace
 
 newtype Candidate = Candidate {name :: String} deriving (Show, Eq)
 newtype Ballot = Ballot { votes :: [Candidate] }
 type Election = [Ballot]
-type CandidateTally = (Candidate, Int)
+type CandidateTally = (Candidate, [Int])
 type ElectionResult = [CandidateTally]
 
 tally :: Election -> ElectionResult
+tally [] = []
 tally e =
-  sortOn (Data.Ord.Down . voteCount) talliedResults
+  case sortedResults of
+    results | leaderVotes >= requiredForMajorityCount -> results
+    results -> trace ("IRV condition detected"++ show results) results
   where
     talliedResults = countBallots e []
+    sortedResults = sortOn (Data.Ord.Down . latestRoundVoteCount) talliedResults
+    totalVotes = sum (map latestRoundVoteCount sortedResults)
+    requiredForMajorityCount = ceiling (fromIntegral totalVotes * 0.5)
+    leaderVotes = latestRoundVoteCount (head sortedResults)
+
 
 countBallots :: Election -> ElectionResult -> ElectionResult
 countBallots [] runningTally = runningTally
 countBallots (Ballot{votes=(firstCandidateVote:_)}:ballots) runningTally =
   case findCandidateResult firstCandidateVote runningTally of
-    Nothing -> countBallots ballots ((firstCandidateVote, 1):filteredRunningTally)
-    Just (firstCandidateVote, votes) -> countBallots ballots ((firstCandidateVote, votes+1):filteredRunningTally)
+    Nothing -> countBallots ballots ((firstCandidateVote, [1]):filteredRunningTally)
+    Just (firstCandidateVote, latestRoundVotes:priorRounds) -> countBallots ballots ((firstCandidateVote, latestRoundVotes+1:priorRounds):filteredRunningTally)
     where
       filteredRunningTally = filter (not . isCandidateTally firstCandidateVote) runningTally
 
@@ -36,5 +45,5 @@ findCandidateResult candidate = find (isCandidateTally candidate)
 isCandidateTally :: Candidate -> CandidateTally -> Bool
 isCandidateTally soughtCandidate (candidate, _) = soughtCandidate == candidate
 
-voteCount :: CandidateTally -> Int
-voteCount (_, count) = count
+latestRoundVoteCount :: CandidateTally -> Int
+latestRoundVoteCount (_, count:_) = count
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,14 +8,28 @@ main = hspec $ do
     it "returns an empty result when no ballots were cast" $ do
       tally [] `shouldBe` []
     it "returns the candidates with their first round vote tallies" $ do
-      tally simpleMajorityPreston `shouldBe` [(preston, 3), (hannah, 1)]
+      tally simpleMajorityPreston `shouldBe` [(preston, [3]), (hannah, [1])]
+    it "redistributes votes from losing candidates when no candidate has a majority" $ do
+      tally instantRunoffPreston `shouldBe` [(preston, [4]), (hannah, [3]), (ruby, [1])]
 
 preston = Candidate "Preston"
 hannah = Candidate "Hannah"
+ruby = Candidate "Ruby"
 simpleMajorityPreston =
   [
     Ballot [preston],
     Ballot [preston],
     Ballot [preston],
     Ballot [hannah]
+  ]
+
+instantRunoffPreston =
+  [
+    Ballot [preston],
+    Ballot [preston],
+    Ballot [preston],
+    Ballot [hannah],
+    Ballot [hannah],
+    Ballot [hannah],
+    Ballot [ruby, preston]
   ]
